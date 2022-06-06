d3e1f81e2e49ae97b5897cf9363e6652e92cfbe9
omar elsahragty <omar@affable.ai>
2022-06-06 23:43:39 +0200

    [EDIT] Format

--- src/helpers/queries.helper.ts
+++ src/helpers/queries.helper.ts
@@ -4,11 +4,15 @@ import { defaultListOptions } from "../../constants";
 
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
-export const buildSearch = (filter: FilterQuery<any>): FilterQuery<any> => ({
-  $or: Object.keys(filter).map(property => ({
-    [property]: typeof filter[property] === "string" ? { $regex: filter[property], $options: "i" } : filter[property],
-  })),
-});
+export const buildSearch = (filter: FilterQuery<any>): FilterQuery<any> =>
+  Object.keys(filter).length > 0
+    ? {
+        $or: Object.keys(filter).map(property => ({
+          [property]:
+            typeof filter[property] === "string" ? { $regex: filter[property], $options: "i" } : filter[property],
+        })),
+      }
+    : {};
 
 export const buildListOptions = (options: ListOptionsInterface): ListOptionsInterface => ({
   ...defaultListOptions,

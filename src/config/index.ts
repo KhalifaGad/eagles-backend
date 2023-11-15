d65d7288568501f7020c11e1fbb448792a755ad3
khalifa3 <khalifa@gobusiness.tech>
2023-11-15 18:06:24 +0200

    feat: update db connection variables

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,13 +1,14 @@
 import { config } from "dotenv";
+
 config();
 
 export default Object.freeze({
   port: parseInt(process.env.PORT || "5000"),
 
   mongoDB: {
-    connectionString: process.env.MONGO_CONNECTION_STRING || "mongodb://127.0.0.1:27017/egypt-eagles",
+    connectionString: process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/egypt-eagles",
   },
 
-  jwtSecret: process.env.JWT_SECRET || "JWT_SECRET",
+  jwtSecret: process.env.COOKIE_SECRET || "JWT_SECRET",
   jwtLifeTime: process.env.JWT_LIFE_TIME || "1y",
 });
